68772875662b926e673b1abea6d751dd36de53d8
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-19 23:15:32 +0100

    Did Course / Python / 2 Functions and Getting Help, added script's code

---
+++ Courses/Python/2_Functions_and_Getting_Help/functions-and-getting-help_script.py
@@ -0,0 +1,11 @@
+def mod_5(x):
+    """Return the remainder of x after dividing by 5"""
+    return x % 5
+
+print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number is the biggest modulo 5?',
+    max(100, 51, 14, key=mod_5),
+    sep='\n',
+)
